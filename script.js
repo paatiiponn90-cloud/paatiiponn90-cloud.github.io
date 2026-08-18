0e309b3b7bb1050ded5133a4d7122512c9d3cfce
paatiiponn90-cloud <paatiiponn90@gmail.com>
2026-08-18 11:25:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//เพิ่ม JavaScript สำหรับการโต้ตอบกับผู้ใช้ (Optional)
 document.addEventListener(’DOMContentLoaded‘,function () {
     document.getElementById('year').innerHTML = new Date().getFullYear();
     console.log(’Portfolio ของ Patiphon พร้อมแล้ว!‘);
